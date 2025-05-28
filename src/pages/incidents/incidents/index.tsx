b0dfee289b804e4cdc7d5c0f5e115f2746e3be74
João Genari <victor.olr@hotmail.com>
2025-05-27 21:12:41 -0300

    refactor: refatora tela de listar incidents para listar oficiais

--- src/pages/incidents/incidents/index.tsx
+++ src/pages/incidents/incidents/index.tsx
@@ -1,78 +1,28 @@
 import { useState } from 'react'
 
+import { getIncidents } from '~/api'
 import { useRouting } from '~/hooks'
 
-import { pageTitle, reduceString, incidentsTbTitle, getStatusColor, getPriorityColor, dateFormatter } from '~/helpers'
+import { pageTitle, reduceString, incidentsTbTitle, setLineColor, dateFormatter } from '~/helpers'
 
 import { Title, Table, Pagination, Button, TableTreatments } from '~/components'
+import { useQuery } from '@tanstack/react-query'
 
 export const Incidents = () => {
 	pageTitle('Chamados')
 
-	const [page, setPage] = useState(1)
-
 	const { handleGoToIncidentDetail, handleGoToIncidentCreate } = useRouting()
 
-	const incidentsTest = [
-		{
-			id: '6135cfe0-5e59-459f-b67c-196c136ed85f',
-			code: 1,
-			title: 'Catraca danificada então você precisa ir lá e atender, pois estamos precisando arrumar emmm',
-			status: {
-				name: 'Aberto',
-			},
-			priority: {
-				name: 'Normal',
-			},
-			category: {
-				name: 'Manutenção de Ambientes',
-			},
-			assigned: {
-				name: null,
-			},
-			created_at: new Date(),
-		},
-		{
-			id: '7135cfe0-5e59-459f-b67c-196c136ed85f',
-			code: 2,
-			title: 'Catraca danificada então você precisa ir lá e atender, pois estamos precisando arrumar emmm',
-			status: {
-				name: 'Aberto',
-			},
-			priority: {
-				name: 'Alta',
-			},
-			category: {
-				name: 'Manutenção de Ambientes',
-			},
-			assigned: {
-				name: null,
-			},
-			created_at: new Date(),
-		},
-		{
-			id: '8135cfe0-5e59-459f-b67c-196c136ed85f',
-			code: 3,
-			title: 'Catraca danificada então você precisa ir lá e atender, pois estamos precisando arrumar emmm',
-			status: {
-				name: 'Aberto',
-			},
-			priority: {
-				name: 'Mediana',
-			},
-			category: {
-				name: 'Manutenção de Ambientes',
-			},
-			assigned: {
-				name: null,
-			},
-			created_at: new Date(),
-		},
-	]
+	const [page, setPage] = useState(1)
+
+	const { data, isPending, isError } = useQuery({
+		queryKey: ['points', page],
+		queryFn: () => getIncidents({ page }),
+	})
 
 	const renderTableBodyRows = () => (
 		<>
-			{incidentsTest.map((incident) => {
+			{data?.body?.payload?.data.map((incident) => {
 				return (
 					<Table.TR
 						key={incident.id}
@@ -81,8 +31,8 @@ export const Incidents = () => {
 					>
 						<Table.TD>{incident.code}</Table.TD>
 						<Table.TD>{reduceString(incident.title, 30)}</Table.TD>
-						<Table.TD className={`text-${getStatusColor(incident.status.name)}`}>{incident.status.name}</Table.TD>
-						<Table.TD className={`text-${getPriorityColor(incident.priority.name)}`}>{incident.priority.name}</Table.TD>
+						<Table.TD>{incident.status.name}</Table.TD>
+						<Table.TD className={`text-${setLineColor(incident.priority.name)}`}>{incident.priority.name}</Table.TD>
 						<Table.TD>{incident?.assigned?.name ? incident.assigned.name : 'A Definir'}</Table.TD>
 						<Table.TD>{dateFormatter.format(new Date(incident.created_at))}</Table.TD>
 					</Table.TR>
@@ -92,9 +42,9 @@ export const Incidents = () => {
 	)
 
 	const TableBody = () => {
-		// if (isPending) return TableTreatments.renderPending(6)
-		// if (isError) return TableTreatments.renderError(6)
-		// if (incidents.data.payload.length === 0) return TableTreatments.renderNoData(6)
+		if (isPending) return TableTreatments.renderPending(6)
+		if (isError) return TableTreatments.renderError(6)
+		if (data?.body?.payload?.data.length === 0) return TableTreatments.renderNoData(6)
 		return renderTableBodyRows()
 	}
 
@@ -104,10 +54,7 @@ export const Incidents = () => {
 				<div className="w-full flex justify-between items-center">
 					<Title title="Chamados" />
 
-					<div className="flex gap-2">
-						<Button description="+ Novo" onClick={handleGoToIncidentCreate} />
-						<Button description="Exportar" />
-					</div>
+					<Button description="+ Novo" onClick={handleGoToIncidentCreate} />
 				</div>
 
 				<Table.Container>
@@ -124,7 +71,12 @@ export const Incidents = () => {
 					</Table.TBody>
 				</Table.Container>
 
-				<Pagination page={page} setPage={setPage} totalCount={Number(1)} totalCountPage={Number(10)} />
+				<Pagination
+					page={page}
+					setPage={setPage}
+					totalCount={Number(data?.body?.payload?.pagination.total)}
+					totalCountPage={Number(data?.body?.payload?.pagination.pages)}
+				/>
 			</div>
 		</main>
 	)
