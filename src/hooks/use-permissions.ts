4ef1cab66c7ba6704cbd66da4fddea702b224906
João Genari <victor.olr@hotmail.com>
2025-06-14 20:55:18 -0300

    refactor: muda método para validar permissões do usuário

--- src/hooks/use-permissions.ts
+++ src/hooks/use-permissions.ts
@@ -1,26 +1,16 @@
 import { useSession } from '~/hooks'
-import { adminRoutes, managerRoutes, funRoutes } from '~/helpers'
 
 export const usePermissions = () => {
 	const { userInfos } = useSession()
 
-	const hasAllPermissionsFromList = (permissionNames: string[]): boolean => {
-		if (!userInfos || !userInfos.profile || !userInfos.profile.permissions) return false
-		return permissionNames.every((permissionName) =>
-			userInfos.profile.permissions.some((perm) => perm.permission === permissionName && perm.active),
-		)
-	}
-	const admScreens = hasAllPermissionsFromList(adminRoutes)
-	const manScreens = hasAllPermissionsFromList(managerRoutes)
-	const funScreens = hasAllPermissionsFromList(funRoutes)
-
 	const hasPermission = (permissionName: string): boolean => {
-		if (!userInfos?.profile?.permissions) return false
-		return userInfos.profile.permissions.some((perm) => perm.permission === permissionName && perm.active)
+		if (!userInfos?.permissions) return false
+		return userInfos.permissions.some((perm) => perm.name === permissionName)
 	}
 
-	const allowedRegisterNews = hasPermission('Register News')
-	const allowedManagementNews = hasPermission('Edit News')
+	const allowedDashboards = hasPermission('Dashboards')
+	const allowedRegisterNews = hasPermission('Cadastrar notícias')
+	const allowedManagementNews = hasPermission('Editar notícias')
 
-	return { admScreens, manScreens, funScreens, allowedRegisterNews, allowedManagementNews }
+	return { allowedDashboards, allowedRegisterNews, allowedManagementNews }
 }
