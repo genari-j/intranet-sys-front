0aeabb1a36ab39f8408a0b4e5cae9211f1145a27
João Genari <victor.olr@hotmail.com>
2025-06-14 21:07:34 -0300

    refactor: melhora todo o processo de rotas abertas e privadas

--- src/routes/routes/routes.tsx
+++ src/routes/routes/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { adminRoutes, managerRoutes } from '~/helpers'
 
-import { PrivateRoutes, PrivateRouteWithPermissions } from '~/routes'
+import { PrivateRoutes, RouteWithPermission } from '~/routes'
 import { AppLayout } from '~/templates'
 
 import * as Pages from '~/pages'
@@ -16,7 +15,7 @@ export const router = createBrowserRouter([
 					{ path: '/entrar', element: <Pages.SignIn /> },
 					{ path: '/', element: <Pages.News /> },
 					{ path: '/sistemas/noticias/:id', element: <Pages.NewsManagement /> },
-					{ path: '/sistemas/chamados/:id', element: <Pages.IncidentManagement /> },
+					{ path: '/FAQ', element: <Pages.FAQ /> },
 				],
 			},
 			{
@@ -24,24 +23,57 @@ export const router = createBrowserRouter([
 				children: [
 					{
 						children: [
-							{ path: '/sistemas', element: <Pages.Systems /> },
-							{ path: '/sistemas/registro-de-ponto', element: <Pages.Points /> },
 							{ path: '/usuarios/:id/perfil', element: <Pages.Profile /> },
-							{ path: '/sistemas/chamados', element: <Pages.Incidents /> },
-							{ path: '/sistemas/chamados/cadastrar', element: <Pages.IncidentsCreate /> },
+							{ path: '/sistemas', element: <Pages.Systems /> },
 							{ path: '/notificacoes', element: <Pages.Notifications /> },
+							{
+								path: '/sistemas/registro-de-ponto',
+								element: <RouteWithPermission permission="Registrar ponto" element={<Pages.Points />} />,
+							},
+							{
+								path: '/sistemas/chamados/cadastrar',
+								element: <RouteWithPermission permission="Registrar chamados" element={<Pages.IncidentsCreate />} />,
+							},
+							{
+								path: '/sistemas/chamados',
+								element: (
+									<RouteWithPermission permission="Visualizar chamados específicos" element={<Pages.Incidents />} />
+								),
+							},
+							{ path: '/sistemas/chamados/:id', element: <Pages.IncidentManagement /> },
+							{
+								path: '/sistemas/noticias/cadastrar',
+								element: <RouteWithPermission permission="Cadastrar notícias" element={<Pages.NewsCreate />} />,
+							},
+							{
+								path: '/dashboard',
+								element: <RouteWithPermission permission="Dashboards" element={<Pages.Dashboard />} />,
+							},
+							{
+								path: '/sistemas/visitas',
+								element: <RouteWithPermission permission="Visualizar visitas" element={<Pages.Visits />} />,
+							},
+							{
+								path: '/sistemas/portaria',
+								element: <RouteWithPermission permission="Visualizar visitas" element={<Pages.Visits />} />,
+							},
+							{
+								path: '/sistemas/estoque',
+								element: <RouteWithPermission permission="Visualizar itens" element={<Pages.Stock />} />,
+							},
+							{
+								path: '/sistemas/ecommerce',
+								element: <RouteWithPermission permission="Visualizar visitas" element={<Pages.Products />} />,
+							},
+							{
+								path: '/sistemas/documentos',
+								element: <RouteWithPermission permission="Visualizar visitas" element={<Pages.Documents />} />,
+							},
+							{
+								path: '/sistemas/compras',
+								element: <RouteWithPermission permission="Visualizar visitas" element={<Pages.Purchases />} />,
+							},
 						],
-					},
-					{
-						element: <PrivateRouteWithPermissions requiredPermissions={managerRoutes} requiredRole="manager" />,
-						children: [
-							{ path: '/FAQ', element: <Pages.FAQ /> },
-							{ path: '/sistemas/noticias/cadastrar', element: <Pages.NewsCreate /> },
-						],
-					},
-					{
-						element: <PrivateRouteWithPermissions requiredPermissions={adminRoutes} requiredRole="admin" />,
-						children: [{ path: '/dashboard', element: <Pages.Dashboard /> }],
 					},
 				],
 			},
