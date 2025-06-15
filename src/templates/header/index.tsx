229a5844a81d2043b24c2525777cded5f6cc9d29
João Genari <victor.olr@hotmail.com>
2025-06-14 21:08:02 -0300

    refactor: ajusta todo o header após ajustes do fluxo de permissões

--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -1,5 +1,6 @@
+import { Fragment } from 'react/jsx-runtime'
 import { useNotification } from '~/contexts'
-import { useLogout, usePermissions, useRouting, useSession } from '~/hooks'
+import { usePermissions, useLogout, useRouting, useSession } from '~/hooks'
 
 import { Button, NavLink } from '~/components'
 
@@ -9,63 +10,55 @@ export const Header = () => {
 	const { unreadCount } = useNotification()
 
 	const { isSignedIn, userInfos } = useSession()
-	const { admScreens, manScreens, funScreens } = usePermissions()
+	const { allowedDashboards } = usePermissions()
 
 	const { handleLogout } = useLogout()
-	const { handleGoToLogin, handleGoToProfile } = useRouting()
+	const { handleGoToLogin } = useRouting()
 
 	return (
 		<header className="w-full flex justify-center fixed z-10 py-4 bg-gray7 shadow-md">
 			<div className="w-full max-w-[1200px] flex justify-between items-center">
 				<div>LOGO</div>
 
-				<div className="flex items-center gap-20">
+				<div className={`flex items-center ${isSignedIn() ? 'gap-20' : 'gap-5'}`}>
 					<nav>
 						<ul className="flex gap-4">
 							<li>
 								<NavLink to="/">Notícias</NavLink>
 							</li>
-							{funScreens && (
-								<li>
-									<NavLink to="/sistemas">Sistemas</NavLink>
-								</li>
+							{isSignedIn() && (
+								<Fragment>
+									<li>
+										<NavLink to="/sistemas">Sistemas</NavLink>
+									</li>
+									{allowedDashboards && (
+										<li>
+											<NavLink to="/dashboard">Dashboard</NavLink>
+										</li>
+									)}
+								</Fragment>
 							)}
-							{admScreens && (
-								<li>
-									<NavLink to="/dashboard">Dashboard</NavLink>
-								</li>
-							)}
-							{manScreens && (
-								<li>
-									<NavLink to="/FAQ">FAQ</NavLink>
-								</li>
-							)}
+							<li>
+								<NavLink to="/FAQ">FAQ</NavLink>
+							</li>
 						</ul>
 					</nav>
 
 					<div className="flex items-center gap-8">
-						<div className="flex items-center gap-2">
-							{isSignedIn() && userInfos?.name && (
-								<>
-									<span
-										title="Perfil"
-										onClick={() => handleGoToProfile(userInfos?.id)}
-										className="cursor-pointer hover:text-gray9 duration-150"
-									>
-										{userInfos?.name.split(' ')[0]}
-									</span>
+						{isSignedIn() && userInfos?.name && (
+							<div className="flex items-center gap-2">
+								<NavLink to={`/usuarios/${userInfos?.id}/perfil`}>{userInfos?.name.split(' ')[0]}</NavLink>
 
-									<NavLink to="/notificacoes" className="relative">
-										<Bell />
-										{unreadCount > 0 && (
-											<span className="h-[22px] w-[22px] flex justify-center items-center absolute top-[-0.6rem] right-[-0.6rem] text-[13px] p-1 text-white9 bg-blue8 rounded-full">
-												{unreadCount}
-											</span>
-										)}
-									</NavLink>
-								</>
-							)}
-						</div>
+								<NavLink to="/notificacoes" className="relative">
+									<Bell />
+									{unreadCount > 0 && (
+										<span className="h-[22px] w-[22px] flex justify-center items-center absolute top-[-0.6rem] right-[-0.6rem] text-[13px] p-1 text-white9 bg-blue8 rounded-full">
+											{unreadCount}
+										</span>
+									)}
+								</NavLink>
+							</div>
+						)}
 
 						{isSignedIn() && userInfos?.name ? (
 							<Button onClick={handleLogout} description="Sair" />
