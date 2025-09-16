232c676ff03a3988f5cdce759b6bb920928e4d42
João Genari <victor.olr@hotmail.com>
2025-09-15 21:01:50 -0300

    feat: better menu layout based on login

--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -4,9 +4,13 @@ import { usePermissions, useLogout, useRouting, useSession } from '~/hooks'
 
 import { Button, NavLink } from '~/components'
 
-import { Bell } from '~/assets'
+import { Bell, Logo } from '~/assets'
+import { Link, useLocation } from 'react-router-dom'
 
 export const Header = () => {
+	const { pathname } = useLocation()
+	const isLoginPath = pathname !== '/entrar'
+
 	const { unreadCount } = useNotification()
 
 	const { isSignedIn, userInfos } = useSession()
@@ -16,57 +20,65 @@ export const Header = () => {
 	const { handleGoToLogin } = useRouting()
 
 	return (
-		<header className="w-full flex justify-center fixed z-10 py-4 bg-gray7 shadow-md">
-			<div className="w-full max-w-[1200px] flex justify-between items-center">
-				<div>LOGO</div>
+		<header className={`w-full flex justify-center fixed z-10 ${isLoginPath ? 'py-2 bg-gray7 shadow-md' : 'pt-10'}`}>
+			<div className={`w-full max-w-[1200px] flex ${isLoginPath ? 'justify-between' : 'justify-center'}  items-center`}>
+				<Link to="/" className="duration-150 transform hover:scale-105">
+					<img
+						src={Logo}
+						alt="Intranet Sys"
+						title="Intranet Sys"
+						className={`w-full ${isLoginPath ? 'max-w-[60px]' : 'max-w-[80px]'}  flex`}
+					/>
+				</Link>
 
-				<div className={`flex items-center ${isSignedIn() ? 'gap-20' : 'gap-5'}`}>
-					<nav>
-						<ul className="flex gap-4">
-							<li>
-								<NavLink to="/">Notícias</NavLink>
-							</li>
-							{isSignedIn() && (
-								<Fragment>
-									<li>
-										<NavLink to="/sistemas">Sistemas</NavLink>
-									</li>
-									{allowedDashboards && (
+				{isLoginPath && (
+					<div className={`flex items-center ${isSignedIn() ? 'gap-20' : 'gap-5'}`}>
+						<nav>
+							<ul className="flex gap-4">
+								<li>
+									<NavLink to="/">Notícias</NavLink>
+								</li>
+								{isSignedIn() && (
+									<Fragment>
 										<li>
-											<NavLink to="/dashboard">Dashboard</NavLink>
+											<NavLink to="/sistemas">Sistemas</NavLink>
 										</li>
-									)}
-								</Fragment>
+										{allowedDashboards && (
+											<li>
+												<NavLink to="/dashboard">Dashboard</NavLink>
+											</li>
+										)}
+									</Fragment>
+								)}
+								<li>
+									<NavLink to="/FAQ">FAQ</NavLink>
+								</li>
+							</ul>
+						</nav>
+
+						<div className="flex items-center gap-8">
+							{isSignedIn() && userInfos?.name && (
+								<div className="flex items-center gap-2">
+									<NavLink to={`/usuarios/${userInfos?.id}/perfil`}>{userInfos?.name.split(' ')[0]}</NavLink>
+
+									<NavLink to="/notificacoes" className="relative">
+										<Bell />
+										{unreadCount > 0 && (
+											<span className="h-[22px] w-[22px] flex justify-center items-center absolute top-[-0.6rem] right-[-0.6rem] text-[13px] p-1 text-white9 bg-blue8 rounded-full">
+												{unreadCount}
+											</span>
+										)}
+									</NavLink>
+								</div>
 							)}
-							<li>
-								<NavLink to="/FAQ">FAQ</NavLink>
-							</li>
-						</ul>
-					</nav>
 
-					<div className="flex items-center gap-8">
-						{isSignedIn() && userInfos?.name && (
-							<div className="flex items-center gap-2">
-								<NavLink to={`/usuarios/${userInfos?.id}/perfil`}>{userInfos?.name.split(' ')[0]}</NavLink>
-
-								<NavLink to="/notificacoes" className="relative">
-									<Bell />
-									{unreadCount > 0 && (
-										<span className="h-[22px] w-[22px] flex justify-center items-center absolute top-[-0.6rem] right-[-0.6rem] text-[13px] p-1 text-white9 bg-blue8 rounded-full">
-											{unreadCount}
-										</span>
-									)}
-								</NavLink>
-							</div>
-						)}
-
-						{isSignedIn() && userInfos?.name ? (
-							<Button onClick={handleLogout} description="Sair" />
-						) : (
-							<Button onClick={handleGoToLogin} description="Login" />
-						)}
+							<Button
+								onClick={isSignedIn() && userInfos?.name ? handleLogout : handleGoToLogin}
+								description={`${isSignedIn() && userInfos?.name ? 'Sair' : 'Entrar'}`}
+							/>
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</header>
 	)
